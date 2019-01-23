351eebac037b7ff801449ff9001bddbc99a75a1f
Daniel Lombraña González <teleyinex@gmail.com>
2019-01-23 23:22:05 +0100

    Add social sharing.

--- components/nav/blog.vue
+++ components/nav/blog.vue
@@ -3,17 +3,44 @@
     v-btn#closebtn(flat='', icon='', @click="goBack")
       v-icon(color='white')
           | mdi-arrow-left
-    v-btn(icon='', flat='', href='https://www.linkedin.com/in/teleyinex')
+    v-btn(icon='', flat='', :href="linkedin" target="blank")
       v-icon(color='white')
         | mdi-linkedin
-    v-btn(icon='', flat='', href='https://twitter.com/teleyinex')
+    v-btn(icon='', flat='', :href="twitter", target="blank")
       v-icon(color='white')
         | mdi-twitter
 </template>
 <script>
 import nav from '~/mixins/nav.js'
+import socialMediaLinks from 'social-media-links'
 export default {
-  mixins: [nav]
+  mixins: [nav],
+  computed: {
+    socialTwitterData() {
+      return {
+        account: 'twitter',
+        url: `https://daniellombrana.es${this.$route.fullPath}`,
+        title: this.$store.state.page.title,
+        img: this.$store.state.page.photoUrl || this.$store.state.heroImg,
+        via: 'teleyinex'
+      }
+    },
+    socialLinkedInData() {
+      return {
+        account: 'linkedIn',
+        url: `https://daniellombrana.es${this.$route.fullPath}`,
+        title: this.$store.state.page.title,
+        img: this.$store.state.page.photoUrl || this.$store.state.heroImg,
+        via: 'teleyinex'
+      }
+    },
+    linkedin() {
+      return socialMediaLinks.create(this.socialLinkedInData)
+    },
+    twitter() {
+      return socialMediaLinks.create(this.socialTwitterData)
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -13,5 +13,6 @@ Vue.use(Vuetify, {
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
     success: colors.green.accent3
-  }
+  },
+  treeShake: true
 })
