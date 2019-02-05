a4cb089c1b0a97075ab638a4e92c7c6688f027db
Daniel Lombraña González <teleyinex@gmail.com>
2019-02-05 10:33:19 +0100

    Track changes.

--- components/bookAday.vue
+++ components/bookAday.vue
@@ -1,5 +1,21 @@
 <template>
-  <v-btn href="https://calendly.com/teleyinex/30min" color="hsla(282, 44%, 27%, 1)" class="ml-0" dark large>
+  <v-btn
+    href="https://calendly.com/teleyinex/30min"
+    color="hsla(282, 44%, 27%, 1)"
+    class="ml-0"
+    dark
+    large
+    @click="open"
+  >
     {{ $t('bookAday') }}
   </v-btn>
 </template>
+<script>
+export default {
+  methods: {
+    open() {
+      this.$ga.event('about', 'button', 'bookAday', 1)
+    }
+  }
+}
+</script>
